e46e9a8b9de0264b8e22e8347975b99676b782bf
DungHueng <dunghoang26lc@gmail.com>
2024-12-12 08:19:52 +0700

    Add files via upload

---
+++ S12-Ex5.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+    char string[] = "Hoang Trung Dung";
+    int length = sizeof(string)/sizeof(char);
+    int count = 0;
+    int inWord = 0;
+
+    for(int i = 0; i < length - 1; i++) {
+        if(string[i] != ' ') {
+            inWord = 1;
+        } else {
+            inWord = 0;
+        }
+
+        if(inWord == 0) {
+            count++;
+        }
+    }
+
+    printf("count = %d\n", count + 1);
+
+
+return 0;	
+}
